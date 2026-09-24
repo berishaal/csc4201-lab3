7f98715fefd26d28975c349232688aeb4ce56c16
berishaal <189380876+berishaal@users.noreply.github.com>
2026-09-24 13:46:32 -0500

    Restore working app

--- main.py
+++ main.py
@@ -10,7 +10,9 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     name = os.environ.get("NAME", "World")
-    return f"Goodbye {name}!"
+    return f"Hello {name}!"
+
+
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
